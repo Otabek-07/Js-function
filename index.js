12955e5fb560fd7b9b84bc5a863c1586a166c2ab
otabek <otajongold@gmail.com>
2024-04-02 13:17:09 +0500

    Masalalar

---
+++ index.js
@@ -0,0 +1,131 @@
+
+/*
+ 1 - Masala
+ function Fasilar(n){
+ 
+ switch (n) {
+    case 1: console.log("Yanvar oyi Qish");
+    break;
+    case 2: console.log("Fevral oyi Qish");
+    break;
+    case 11: console.log("Noyabir oyi Qish");
+    break;
+    case 12: console.log("Dekabir Qish");
+    break;
+    case 3: console.log("Mart oyi Baxor");
+    break;
+    case 4: console.log("Aprel oyi Baxor");
+    break;
+    case 5: console.log("May oyi Baxor");
+    break;
+    case 6: console.log("Iyun oyi Yoz");
+    break;
+    case 7: console.log("Iyul oyi Yoz");
+    break;
+    case 8: console.log("Avgust oyi Yoz");
+    break;
+    break;
+    case 9: console.log("Sentyabir oyi Kuz");
+    break;
+    case 10: console.log("Oktiyabir oyi Kuz");
+    break;
+ 
+    default: console.log("Faslarn to'g'ri kiriting");
+        break;
+ }
+ };
+
+ Fasilar(5);
+ *
+
+ 2 - Masala
+ Birinchi usul
+function Age(n) {
+    return 2024 - n;
+
+}
+console.log(Age(2010));
+
+
+
+ikkinchi usul kutubxonalar bilan ishlasa anik ishlaydi
+
+/*function Yosh (yil, oy, kun){
+
+     if( oy < 10 ||  oy == 10 && kun <= 7){
+       return 2024 - yil;
+     } else{
+       return 2024 - yil - 1;
+     }
+
+}
+
+console.log(Yosh( 1995, 10, 7));*
+
+
+ 3 - Masala
+ function Son(n){
+    let juft = 0;
+    let toq = 0;
+
+    for (let i = 1; i < n; i++) {
+         
+         if ( i % 2 == 0) {
+            juft +=i;
+         }else{
+            toq +=i;
+         }
+        
+    }
+
+    return juft /toq;
+ }
+
+ console.log(Son(10));
+ 4 - Masala
+ Birinchi usul
+ function Kanpaniya(n){
+     if( n === +99899){
+         console.log("Uztelecom");
+       }else if( n === +99895){
+         console.log("Uztelecom");
+       }else if( n === +99877){
+         console.log("Uztelecom");
+       }else if( n === +99890){
+         console.log("Beeline");
+       }else if( n === +99891){
+         console.log("Beeline");
+       }else if(n === +99894){
+         console.log("Ucel");
+       }else if( n === +99893){
+        console.log("Ucel");
+       }else{
+          console.log("Bunday Kanpaniya yo'q");
+       }
+ }
+
+ console.log(Kanpaniya(+99893));
+  Ikkinchoi usl
+  function Aloqa(n){
+      switch (n){
+          case +99899:console.log("Uztelecom");
+          break;
+          case +99895:console.log("Uztelecom");
+          break;
+          case +99877:console.log("Uztelecom");
+          break;
+          case +99894:console.log("Ucel");
+          break;
+          case +99893:console.log("Ucel");
+          break;
+          case +99890:console.log("Beeline");
+          break;
+          case +99891:console.log("Beeline");
+          break;
+          default : console.log("Bunday malumot yo'q");
+
+      }
+  }
+
+  console.log(Aloqa(+99895));*/
+
